45f16aecb4de7f5d72373dbe9939bbeaa131a3fc
Shrutika <gshrutika91@gmail.com>
2020-03-02 10:14:05 +0530

    structural directoies

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-dream-app';
+  welcomeMessage='I am a ISSC student';
+  score = 100;
+
+  student = {
+    name:'shrutika',
+    age: 21
+  };
+
+  students = ['Rohan','adu','raju','nikita','preksha'];
 }
