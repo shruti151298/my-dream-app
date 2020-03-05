052d8ca0c94170d1a34957fb63b235b2db5d54e3
Shrutika <gshrutika91@gmail.com>
2020-03-05 10:27:23 +0530

    Atrribute directuve

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -7,13 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-dream-app';
-  welcomeMessage='I am a ISSC student';
+  welcomeMessage= 'I am a ISSC student';
   score = 100;
 
   student = {
-    name:'shrutika',
+    name: 'shrutika',
     age: 21
   };
 
-  students = ['Rohan','adu','raju','nikita','preksha'];
+  students = ['Rohan', 'adu', 'raju', 'nikita', 'preksha'];
+  teachers = [
+    {
+      name : 'Bedekar mam' ,
+      subject : 'Numerical Method'
+    },
+     {
+      name: 'Tondon sir' ,
+      subject: 'CPP'
+
+    }
+  ];
+  day = 'Sunday';
+  showWhiteText = false;
+  showBoldText = false;
+
+  facebookTmageLink = './assets/download.jpeg' ;
+  
+
+  sayhello() {
+    console.log("hello");
+    this.showWhiteText = !this.showWhiteText;
+    this.showBoldText = !this.showBoldText;
+    
+
+  }
 }
